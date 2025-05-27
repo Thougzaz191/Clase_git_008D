7c39535b82316d08f3d52194b283421a5b6ce5a6
Thougzaz191 <gabrielthekiler.1987@gmail.com>
2025-05-27 12:18:46 -0400

    Esta es la segunda version

--- programa_ejemplo_eva3.py
+++ programa_ejemplo_eva3.py
@@ -1,5 +1,6 @@
 usuarios= {}
 op = 1
+v_variable = 0
 while op < 3:
     usuario_ok=False
     print("1._iniciar sesion:")
@@ -28,6 +29,5 @@ while op < 3:
         case 3:
                 print("Cierre de sesion")              
     if  usuario_ok == True:
-        
 
-print(usuarios)
+        print(usuarios)
